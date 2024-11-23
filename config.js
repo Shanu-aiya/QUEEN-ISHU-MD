364d90169690231afe822ee322f5d157278287a8
Shanu-aiya <aiyashanu87@gmail.com>
2024-11-23 23:34:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "W6x3iBKB#P92yF1LbPVTiCPYwVDrXcib5ns12s4jNrrTVCp77IRQ",
+SESSION_ID: process.env.SESSION_ID || "y2BnRDia#nsAhgXHbuAweOzJFmN5FrHOTJpaub40DwkS4hrz11T8",
 MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/"
 };
